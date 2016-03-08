cbd9b1ff5fa524131d17f4b7cd80cc439d61e9f5
Ed Moore <eddie.moore@gmail.com>
2016-03-08 11:10:27 +0800

    no-case-declarations

--- packages/eslint-config-migme/rules/best-practices.js
+++ packages/eslint-config-migme/rules/best-practices.js
@@ -13,7 +13,7 @@ module.exports = {
     'guard-for-in': 2,
     'no-alert': 1,
     'no-caller': 2,
-    'no-case-declarations': 0,
+    'no-case-declarations': 2,
     'no-div-regex': 0,
     'no-else-return': 2,
     'no-empty-function': 0,
