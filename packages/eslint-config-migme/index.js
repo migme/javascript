af90b67781504c0eab86425023791dee5345499d
Ed Moore <eddie.moore@gmail.com>
2016-03-21 14:30:05 +0800

    Added rules for react

--- packages/eslint-config-migme/index.js
+++ packages/eslint-config-migme/index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'eslint-config-migme/base',
     'eslint-config-migme/rules/strict',
+    'eslint-config-migme/rules/react',
   ].map(require.resolve),
   rules: {}
 }
