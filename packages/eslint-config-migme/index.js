36714bf3ed8e045993507148690213dfe884124d
Ed Moore <eddie.moore@gmail.com>
2016-07-05 11:36:33 +0800

    Added new properties and upgraded to eslint v3

--- packages/eslint-config-migme/index.js
+++ packages/eslint-config-migme/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   extends: [
-    'eslint-config-migme/base',
-    'eslint-config-migme/rules/strict',
-    'eslint-config-migme/rules/react',
+    './base',
+    './rules/strict',
+    './rules/react',
   ].map(require.resolve),
-  rules: {}
+  rules: {},
 }
--- packages/eslint-config-migme/legacy.js
+++ packages/eslint-config-migme/legacy.js
@@ -1,20 +1,20 @@
 module.exports = {
-	extends: [
-		'eslint-config-migme/rules/best-practices',
-		'eslint-config-migme/rules/errors',
-		'eslint-config-migme/rules/legacy',
-		'eslint-config-migme/rules/node',
-		'eslint-config-migme/rules/style',
-		'eslint-config-migme/rules/variables',
-	].map(require.resolve),
-	env: {
-		browser: true,
-		node: true,
-		amd: false,
-		mocha: false,
-		jasmine: false
-	},
-	ecmaFeatures: {},
-	globals: {},
-	rules: {}
+  extends: [
+    './rules/best-practices',
+    './rules/errors',
+    './rules/legacy',
+    './rules/node',
+    './rules/style',
+    './rules/variables',
+  ].map(require.resolve),
+  env: {
+    browser: true,
+    node: true,
+    amd: false,
+    mocha: false,
+    jasmine: false,
+  },
+  ecmaFeatures: {},
+  globals: {},
+  rules: {},
 }
